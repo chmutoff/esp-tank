ce1f5c46d8f762515ade0270197ed5b2864460b7
chmutoff <sir.antoxic@gmail.com>
2022-02-06 17:07:53 +0100

    flash_led.cpp: replaced print with log. bugfix.

--- lib/flash_led/flash_led.cpp
+++ lib/flash_led/flash_led.cpp
@@ -8,7 +8,7 @@ int flash_led_init(flash_led_config_t *config)
 {
     if (ledcSetup(config->ledc_chan, FLASH_LED_PWM_FREQUENCY, FLASH_LED_PWM_RESOLUTION) == 0) // configure LED PWM channel
     {
-        Serial.printf("Failed to set up ledc channel %d\n", config->ledc_chan);
+        log_e("Failed to set up ledc channel %d\n", config->ledc_chan);
         return -1;
     }
     ledcWrite(config->ledc_chan, 0);               // turn off by default
@@ -22,25 +22,25 @@ int flash_led_set_brightness(int val)
     if (val == 0)
     {
         ledcWrite(_ledc_chan, 0);
-        Serial.println("Flash led OFF");
+        log_v("Flash led OFF");
         return 0;
     }
     else if (val == 100)
     {
         ledcWrite(_ledc_chan, FLASH_LED_PWM_MAX_VALUE);
-        Serial.println("Flash led 100%");
-        return 100;
+        log_v("Flash led 100%, pwm %d\n", FLASH_LED_PWM_MAX_VALUE);
+        return FLASH_LED_PWM_MAX_VALUE;
     }
     else if (val > 0 && val < 100)
     {
         int brightness = round((pow(2, (1 + (val * 0.02))) - 2) / 6 * FLASH_LED_PWM_MAX_VALUE);
         ledcWrite(_ledc_chan, brightness);
-        Serial.printf("Flash led %d%%, pwm %d\n", val, brightness);
+        log_v("Flash led %d%%, pwm %d\n", val, brightness);
         return brightness;
     }
     else
     {
-        Serial.printf("Flash led received wrong brightnes value: %d\n", val);
+        log_e("Flash led received wrong brightnes value: %d\n", val);
         return -1;
     }
 }
